cb95e5776d9aa5b98a6d66f7d7c6aeaf61d34981
luca-de-bar <lucadebar33@gmail.com>
2024-05-07 23:44:34 +0200

    add : final exercise

---
+++ main.js
@@ -0,0 +1,33 @@
+﻿import "./style.css";
+
+("use strict");
+
+const km = prompt("Quanti KM vuoi percorrere?");
+const age = prompt("Quanti anni hai?");
+const kmNumeric = parseFloat(km); 
+const ageNumeric = parseInt(age); 
+
+if (isNaN(kmNumeric) || isNaN(ageNumeric)) {
+  console.log("Inserisci valori numerici validi per KM e per l'età.");
+} else {
+  const pricePerKm = 0.21;
+  const formula = kmNumeric * pricePerKm;
+
+  if (ageNumeric < 18) {
+    const discount = (formula * 20) / 100;
+    const discountedPrice = formula - discount;
+    console.log(
+      `Hai diritto ad uno sconto del 20%, il prezzo originale è di ${formula.toFixed(2)}€ ma tu pagherai solamente ${discountedPrice.toFixed(2)}€`,
+    );
+  } else if (ageNumeric >= 65) {
+    const discount = (formula * 40) / 100;
+    const discountedPrice = formula - discount;
+    console.log(
+      `Hai diritto ad uno sconto del 40%, il prezzo originale è di ${formula.toFixed(2)}€ ma tu pagherai solamente ${discountedPrice.toFixed(2)}€`,
+    );
+  } else {
+    console.log(
+      `Non hai diritto a nessuno sconto, il prezzo totale è di ${formula.toFixed(2)}€`,
+    );
+  }
+}
