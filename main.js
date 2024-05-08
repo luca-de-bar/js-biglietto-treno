c53cabc28b8ecd969fa01c891e080799db35dc24
luca-de-bar <lucadebar33@gmail.com>
2024-05-08 22:56:58 +0200

    add : a prova di proiettile

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const age_int = parseInt(age);
 // Validazione input, se age buono, procede con KM e valida anche quello.
 if (isNaN(age) || age === age_int) {
     window.alert("Inserisci un valore numerico (ES : 20,18,55)");
-} else if (age_int <= 0) {
+} else if (age_int <= 0 || age_int >= 110) {
     window.alert("Inserisci un'età valida");  
 } else if (age === null || age.trim() === ""){
    window.alert("Inserisci un valore.")
@@ -15,8 +15,10 @@ else {
 
     if (isNaN(KM) || KM === KM_float) {
         window.alert("Inserisci un valore numerico (ES: 200,123.30,500)");
-    } else if (KM_float <= 0) {
-        window.alert("Inserisci un numero di KM valido");
+    } else if (KM_float <= 0 || KM_float>=999) {
+        window.alert("Non puoi fare 0KM o meno..e se hai inserito un numero assurdo" +
+            " ricorda che è un treno, non un aereo."
+        );
     } else if (KM === null || KM.trim() === "") {
         window.alert("Inserisci un valore.")
     }    
